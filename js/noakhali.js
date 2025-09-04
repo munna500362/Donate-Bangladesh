357935bd8023ba89fffc41d22b3492ee696178dc
mehedi hasan <mehedihasan500362@gmail.com>
2025-09-04 22:35:03 +0600

    Noakhali section complete without history

--- js/noakhali.js
+++ js/noakhali.js
@@ -1,24 +1,19 @@
 document.getElementById('donate-noakhali-btn').addEventListener('click', function(event){
     event.preventDefault()
-    console.log("Hellow")
     let NoakhaliInput = getDonateInputById("input-noakhali");
-    console.log("Noakhali Input Money :", NoakhaliInput);
     let Amount = getDonateFieldById("my-money");
    if(NoakhaliInput > 0 && NoakhaliInput< Amount){
-    console.log(" This is right");
 
     
     document.getElementById("congration-noakhali").showModal();
+
     let NoakhaliDonate = getDonateFieldById("noakhali-donate");
     let NewNoakhaliDonation = NoakhaliDonate + NoakhaliInput;
     document.getElementById("noakhali-donate").innerText = NewNoakhaliDonation;
-    console.log(NewNoakhaliDonation);
 
-    
    
     let FinalAmount = Amount-NoakhaliInput;
     document.getElementById("my-money").innerText = FinalAmount;
-    console.log("Final Amount :", FinalAmount);
 
     // const div = document.createElement('div');
     // div.classList.add("noakhali-history", "bg-green-500", "hidden");
